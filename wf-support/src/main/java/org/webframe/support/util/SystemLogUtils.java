48d2d922c489b2faaa6039a478df908e3743cd1e
Guoqing Huang <guoqing.huang@foxmail.com>
2012-02-13 09:40:29 +0800

    #78 wf-support 完善SystemLog日志输出

--- wf-support/src/main/java/org/webframe/support/util/SystemLogUtils.java
+++ wf-support/src/main/java/org/webframe/support/util/SystemLogUtils.java
@@ -1,6 +1,8 @@
 
 package org.webframe.support.util;
 
+import java.util.Date;
+
 /**
  * @author <a href="mailto:guoqing.huang@foxmail.com">黄国庆 </a>
  * @version $Id: codetemplates.xml,v 1.1 2009/09/07 08:48:12 Exp $ Create: 2011-3-19 下午05:50:53
@@ -9,15 +11,17 @@ public final class SystemLogUtils {
 
 	public static boolean	enableSystemLog	= true;
 
+	public static long		currentTime			= new Date().getTime();
+
 	private SystemLogUtils() {
 	}
 
 	public static void println(Object msg) {
-		if (enableSystemLog) System.out.println(msg);
+		if (enableSystemLog) System.out.println(timeMsg("[INFO]", msg));
 	}
 
 	public static void errorPrintln(Object msg) {
-		if (enableSystemLog) System.err.println("[ERROR] " + msg);
+		if (enableSystemLog) System.err.println("[ERROR]" + msg);
 	}
 
 	public static void rootPrintln(Object msg) {
@@ -30,5 +34,14 @@ public final class SystemLogUtils {
 
 	public static void thirdPrintln(Object msg) {
 		println("    " + msg);
+	}
+
+	private static String timeMsg(String level, Object msg) {
+		long time = new Date().getTime();
+		try {
+			return level + (time - currentTime) + " ms " + msg;
+		} finally {
+			currentTime = time;
+		}
 	}
 }
